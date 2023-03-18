9eed1d4551f4e322c23e97b109249232d6b1ab04
DmytroZasl <zaslavsky.dima20@gmail.com>
2023-03-18 11:56:41 +0200

    fix files with linters

--- tests/services/transaction_filters/test_init.py
+++ tests/services/transaction_filters/test_init.py
@@ -1,6 +1,6 @@
 import pytest
 
-from od_search.config.constants import TransactionFilterName
+from od_search.config.constants import TransactionFilterNameOrderfulFormat
 from od_search.services.transaction_filters import (
     get_all_transactions_filters,
     get_transaction_filter_by_name,
@@ -9,7 +9,7 @@ from od_search.services.transaction_filters import (
 
 def test_get_all_transactions_filters() -> None:
     all_filters = get_all_transactions_filters()
-    for name in TransactionFilterName:
+    for name in TransactionFilterNameOrderfulFormat:
         if name not in all_filters:
             raise AssertionError(f"Can not find transaction filter by name: {name.value!r}")
 
@@ -19,12 +19,12 @@ def test_get_all_transactions_filters() -> None:
 @pytest.mark.parametrize(
     "filter_name",
     [
-        TransactionFilterName.LX_LOOP_AT7_LOOP,
-        TransactionFilterName.S5_LOOP,
-        TransactionFilterName.BUSINESS_REFERENCE_NUMBER,
+        TransactionFilterNameOrderfulFormat.LX_LOOP_AT7_LOOP,
+        TransactionFilterNameOrderfulFormat.S5_LOOP,
+        TransactionFilterNameOrderfulFormat.BUSINESS_REFERENCE_NUMBER,
     ],
 )
-def test_get_transaction_filter_by_name(filter_name: TransactionFilterName) -> None:
+def test_get_transaction_filter_by_name(filter_name: TransactionFilterNameOrderfulFormat) -> None:
     transaction_filter = get_transaction_filter_by_name(name=filter_name)
     assert transaction_filter
 
